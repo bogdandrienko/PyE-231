2911e8343f25fcf22211d673eba80007574c31b4
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-05-17 22:13:47 +0600

    update

---
+++ projects/1/resume/new2.py
@@ -0,0 +1,11 @@
+val1 = 12
+val2 = 13
+
+if val1 > 100 and (val2 > 100 or not val2 > 100):
+    print("Оба значения подходят")
+else:
+    print("Значения не подходят")
+
+bool1 = True
+if not bool1:
+    print("Всё в порядке")
