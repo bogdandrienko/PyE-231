25a6ded70645ed1930c6ae22dc4a0f1153cd9d53
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2024-03-13 22:06:27 +0500

    update

---
+++ projects/8/go_web/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	a := 12
+	a = 666
+	//a = "11"
+	fmt.Println(a)
+	fmt.Println("Hello World!")
+
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("0.0.0.0:8001")
+}
